aaa80b982dcf03caba536505a8374ae3648b0fd5
Voldemert1337 <maxsteel0806@gmail.com>
2024-11-05 14:29:47 +0300

    Firs commit

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import index, functionality
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', index),
+    path('func/', functionality),
+
 ]
